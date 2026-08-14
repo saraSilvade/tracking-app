e22b831246c716f730f865e9ae3558db78003e93
saraSilvade <saraleepark1994@gmail.com>
2026-08-14 15:49:05 +0200

    set up tsconfig files, user mockdata

---
+++ src/data/initialData.ts
@@ -0,0 +1,13 @@
+
+import type { UserProfile } from "../types";
+
+export const initialUser: UserProfile = {
+name: 'Iris',
+email: 'iris22@gmail.com', 
+avatar: '⚔️',
+level: 3, 
+exp: 15,
+expMax:20,
+gold: 100,
+gems: 200
+}
---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+
+
+export interface UserProfile {
+    name: string;
+    email: string;
+    avatar: string;
+    level: number;
+    exp: number; 
+    expMax: number;
+    gold: number; 
+    gems: number;
+}
